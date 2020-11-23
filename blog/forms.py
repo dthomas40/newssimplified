41db8a0d59cc7b80cf1e267389e252a51f4bc2eb
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2020-11-23 00:08:16 -0700

    added form for test

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ArticlesForm(forms.Form):
-    article1 = forms.CharField(widget=forms.Textarea placeholder='Article One')
-    article2 = forms.CharField(widget=forms.Textarea placeholder='Article Two')
+    article1 = forms.CharField(widget=forms.Textarea)
+    article2 = forms.CharField(widget=forms.Textarea)
