371fa9cec449d82332ce461a01a280300cec75a1
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2020-11-23 00:18:13 -0700

    radical clean up. again.

--- blog/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class ArticlesForm(forms.Form):
-    article1 = forms.CharField(widget=forms.Textarea)
-    article2 = forms.CharField(widget=forms.Textarea)
--- blog/views.py
+++ blog/views.py
@@ -45,24 +45,6 @@ posts = [
     }
 ]
 
-def get_articles(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = ArticlesForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = ArticlesForm()
-
-    return render(request, 'base.html', {'form': form})
-
 def home(request):
     context = {
         'posts': posts
