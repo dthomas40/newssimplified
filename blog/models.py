319dc55839f72cb0df700ca759c560c7ad2c449a
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2021-04-10 01:42:53 -0700

    PostManager fix

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 # Create your models here.
 
 class PostManager(models.Manager):
-    def create_book(self, title):
-        post = self.create(title=title)
+    def create_post(self, title, content):
+        post = self.create(title=title, content=content)
         # do something with the book
         return post
 
@@ -17,4 +17,4 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-post = Post.objects.create_book("Pride and Prejudice")
+post = Post.objects.create_post("Pride and Prejudice","Nothing to see here")
