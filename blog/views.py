f4bb6850ef97a385946c6eaccc4a90fcbcc780b7
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2021-04-11 03:13:06 -0700

    Post changes, genre added

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from subprocess import run, PIPE
 import sys
 from .models import Post
+from .parser import ProcessManager
 # Create your views here.
 
 def button(request):
@@ -21,14 +22,16 @@ def external(request):
     input1 = request.POST.get('article1')
     input2 = request.POST.get('article2')
     out = run([sys.executable,'similaritycheck.py', input1,input2], shell=False, stdout=PIPE)
+    parser_out = ProcessManager.process_article(input1)
     print(out)
 
     context = {
         'posts': Post.objects.all(),
-        'data1': out.stdout
+        'data1': out.stdout,
+        'data2': parser_out
     }
 
-    Post.objects.create_post(title, out, genre[0])
+    Post.objects.create_post(title, parser_out, genre[0])
 
     return render(request,'blog/home.html',context)
 
