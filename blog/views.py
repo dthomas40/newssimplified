f80ce90bda0b52a9343af4b88641e642b64deb60
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2021-04-20 00:29:15 -0700

    automate.py created

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,9 @@ def home(request):
 
     parser_out = output.Formatter.format(process1, process2, process3)
 
+    title = "Automation Debugging: Test Run"
+    genre = "World"
+
     Post.objects.create_post(title, parser_out, genre[0].capitalize())
 
     return render(request, 'blog/home.html', context)
