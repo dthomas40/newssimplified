c9ccd401e8d249607e7f4942a18763c5bba78dd0
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2021-04-19 23:05:42 -0700

    output.py edited with string return

--- blog/output.py
+++ blog/output.py
@@ -1,4 +1,5 @@
 
 class Formatter():
     def format(parser_output1, parser_output2, similarity_checker_output):
-        return parser_output1+parser_output2+similarity_checker_output
+        completed_output = parser_output1+parser_output2+similarity_checker_output
+        return completed_output
