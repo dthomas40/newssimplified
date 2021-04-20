f7c178a5738600db2f957f5086945ce9636407c9
atomy1 <tomyalwin4@gmail.com>
2021-04-19 23:35:51 -0700

    changing views

--- blog/views.py
+++ blog/views.py
@@ -19,10 +19,43 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 def external(request):
+    page = requests.get('https://news.google.com/topstories?hl=en-US&gl=US&ceid=US%3Aen')
+    bSoup = BeautifulSoup(page.content, 'html.parser')
+    links_list = bSoup.find_all('a')
+    links = []
+    for link in links_list:
+        if 'href' in link.attrs:
+            links.append(str(link.attrs['href']) + '\n')
+
+    i = 0
+    art1_url = links[i]
+    while art1_url[:10] != './articles':
+        art1_url = links[i]
+        i += 1
+
+    art2_url = art1_url
+
+    while art1_url == art2_url or './publications' in art2_url or './topics' in art2_url or './stories' in art2_url:
+        art2_url = links[i]
+        i += 1
+
+    art1_url = 'https://news.google.com/' + art1_url[2:]
+    art2_url = 'https://news.google.com/' + art2_url[2:]
+
+    article1 = Article(art1_url)
+    article2 = Article(art2_url)
+    article1.download()
+    article1.parse()
+    article1.nlp()
+    article2.download()
+    article2.parse()
+    article2.nlp()
+
+
     genre = request.POST.get('genre')
     title = request.POST.get('title')
-    input1 = request.POST.get('article1')
-    input2 = request.POST.get('article2')
+    input1 = article1
+    input2 = article2
 
     process1 = parser.ProcessManager.process_article(input1)
     process2 = parser.ProcessManager.process_article(input2)
