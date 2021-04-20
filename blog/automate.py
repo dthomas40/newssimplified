84c6d0a094446ef7fb0b7b25699480092f366f37
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2021-04-19 23:55:01 -0700

    automate.py created

---
+++ blog/automate.py
@@ -0,0 +1,37 @@
+
+class Scraper():
+    def find_link():
+        page = requests.get('https://news.google.com/topstories?hl=en-US&gl=US&ceid=US%3Aen')
+        bSoup = BeautifulSoup(page.content, 'html.parser')
+        links_list = bSoup.find_all('a')
+        links = []
+        for link in links_list:
+            if 'href' in link.attrs:
+                links.append(str(link.attrs['href']) + '\n')
+
+        i = 0
+        art1_url = links[i]
+        while art1_url[:10] != './articles':
+            art1_url = links[i]
+            i += 1
+
+        art2_url = art1_url
+
+        while art1_url == art2_url or './publications' in art2_url or './topics' in art2_url or './stories' in art2_url:
+            art2_url = links[i]
+            i += 1
+
+        art1_url = 'https://news.google.com/' + art1_url[2:]
+        art2_url = 'https://news.google.com/' + art2_url[2:]
+
+        article1 = scrape(art1_url)
+        article2 = scrape(art2_url)
+
+        return (article1,article2)
+
+    def scrape(art_url):
+        article = Article(art_url)
+        article.download()
+        article.parse()
+        article.nlp()
+        return article
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 from . import parser
 from . import similaritycheck
 from . import output
+from . import automate
 # Create your views here.
 
 def button(request):
@@ -16,42 +17,12 @@ def home(request):
         'posts': Post.objects.all()
     }
     print(request.POST)
+
+    automate.Scraper.find_link()
+
     return render(request, 'blog/home.html', context)
 
 def external(request):
-    page = requests.get('https://news.google.com/topstories?hl=en-US&gl=US&ceid=US%3Aen')
-    bSoup = BeautifulSoup(page.content, 'html.parser')
-    links_list = bSoup.find_all('a')
-    links = []
-    for link in links_list:
-        if 'href' in link.attrs:
-            links.append(str(link.attrs['href']) + '\n')
-
-    i = 0
-    art1_url = links[i]
-    while art1_url[:10] != './articles':
-        art1_url = links[i]
-        i += 1
-
-    art2_url = art1_url
-
-    while art1_url == art2_url or './publications' in art2_url or './topics' in art2_url or './stories' in art2_url:
-        art2_url = links[i]
-        i += 1
-
-    art1_url = 'https://news.google.com/' + art1_url[2:]
-    art2_url = 'https://news.google.com/' + art2_url[2:]
-
-    article1 = Article(art1_url)
-    article2 = Article(art2_url)
-    article1.download()
-    article1.parse()
-    article1.nlp()
-    article2.download()
-    article2.parse()
-    article2.nlp()
-
-
     genre = request.POST.get('genre')
     title = request.POST.get('title')
     input1 = article1
