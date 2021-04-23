066f763cf1c5a8d04fe21905c09fcdd240308d4f
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2021-04-23 03:52:13 -0700

    styling changes

--- blog/automate.py
+++ blog/automate.py
@@ -35,18 +35,17 @@ class Scraper():
 
         article1 = Article(art1_url)
         article2 = Article(art2_url)
-        article1.download()
-        article1.parse()
-        article1.nlp()
-        time.sleep(100)
-
-        article2.download()
-        article2.parse()
-        article2.nlp()
 
         while article2.text == "":
             while art1_url == art2_url or './publications' in art2_url or './topics' in art2_url or './stories' in art2_url:
                 art2_url = links[i]
                 i += 1
+
+        article1.download()
+        article1.parse()
+        article1.nlp()
+        article2.download()
+        article2.parse()
+        article2.nlp()
 
         return (article1.title + " vs " + article2.title, article1.authors, article2.authors, article1.text, article2.text, art1_url, art2_url)
